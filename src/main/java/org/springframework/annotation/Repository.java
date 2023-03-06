46e2d08d31da7e509709eb6cdc86e291e0e56c72
neilCao <1935621364@qq.com>
2023-03-06 22:35:39 +0800

    创建组件注册注解

---
+++ src/main/java/org/springframework/annotation/Repository.java
@@ -0,0 +1,18 @@
+package org.springframework.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Date：2023/3/6
+ * Description：Repository注解
+ *
+ * @author neilCao
+ * @version 1.0
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Repository {
+}
